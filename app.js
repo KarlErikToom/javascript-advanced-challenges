aa81bb910c7320e29f7155dcc4da6f43834858cc
Karl <toom.karl42@gmail.com>
2023-04-27 13:53:34 +0300

    q6 ALMOST GOT IT

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-async function postsByUser(userId) {
-  const promise = await fetch("https://jsonplaceholder.typicode.com/posts");
+async function firstSixIncomplete() {
+  const promise = await fetch("https://jsonplaceholder.typicode.com/todos");
 
   const result = await promise.json();
 
-  const posts = result.filter((elem) => elem.userId === userId);
-  
-  console.log(posts)
+  const toDos = result.filter(elem => !elem.completed).slice(0 ,6);
+
+  console.log(toDos);
 }
 
-postsByUser(4);
+firstSixIncomplete ();
