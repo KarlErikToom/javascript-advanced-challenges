d6340e7e4409f8365e4530138a22b92b1c4d789e
Karl <toom.karl42@gmail.com>
2023-05-03 11:50:36 +0300

    Q6 kinda got it

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-async function postsByUser(userId){
+/*async function postsByUser(userId){
  const promise = await fetch ("https://jsonplaceholder.typicode.com/posts")
 const result = await promise.json()
 
@@ -6,4 +6,13 @@ const posts = result.filter(elem => elem.userId === userId)
 console.log(posts)
 }
 
-postsByUser(4)
+postsByUser(4)*/
+
+async function firstSixIncomplete(toDo) {
+  const promise = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const result = await promise.json();
+  const incompleteTasks = result.filter(elem => !elem.completed).slice(0, 6)
+  console.log(incompleteTasks)
+}
+
+firstSixIncomplete(2);
